c14935ccb44281a1c6451efd074e9d76ed0949b8
taro.kirameki <taro.kirameki@techacademy.jp>
2023-02-16 17:48:36 +0900

    show, エディット、サーブレットの作成

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public ShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,14 +33,13 @@ public class IndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
+        Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+        em.close();
 
-    List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-     em.close();
+        request.setAttribute("task", m);
 
-     request.setAttribute("tasks", tasks);
-
-     RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-     rd.forward(request, response);
-     }
+         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
+            rd.forward(request, response);
+    }
 
 }
